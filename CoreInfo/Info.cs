db98cc9db8350bb12fc8b7b2fe42d672bfe7ed3c
PowerAngelXD <692732163@qq.com>
2023-07-22 17:51:16 +0800

    Update: New Core Version: RELEASE-2023722-0100

--- CoreInfo/Info.cs
+++ CoreInfo/Info.cs
@@ -2,7 +2,7 @@
 
 public class Info
 {
-    public const string CoreVersion = "ALPHA-2023721-0001";
+    public const string CoreVersion = "RELEASE-2023722-0100";
     public const string LauncherVersion = "ALPHA 0.1";
     public const string License = "MIT";
 }
