10ae8b31fc77f789359c4689fbc293d14ecb0b79
PowerAngelXD <692732163@qq.com>
2023-07-22 17:49:27 +0800

    Update: Argument Manager

---
+++ Command/ArgumentManager.cs
@@ -0,0 +1,24 @@
+﻿namespace SLCore.Command;
+
+public class ArgumentManager
+{
+    public static string MergeArgs(string[]? segs)
+    {
+        string ret = new string((ReadOnlySpan<char>)"");
+        if (segs != null)
+        {
+            foreach (var seg in segs)
+            {
+                ret += seg + " ";
+            }
+
+            ret = ret.Substring(0, ret.Length - 1);
+        }
+        return ret;
+    }
+
+    public static string[]? ConsumeHead(string[]? segs)
+    {
+        return segs?.Skip(1).ToArray();
+    }
+}
