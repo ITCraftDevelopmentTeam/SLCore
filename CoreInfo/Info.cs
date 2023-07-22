5e62ee935f4eb0ef8a3d75d35f0ff4f4a26fb831
PowerAngelXD <692732163@qq.com>
2023-07-22 17:50:03 +0800

    Update: Core and Launcher information

---
+++ CoreInfo/Info.cs
@@ -0,0 +1,8 @@
+﻿namespace SLCore.CoreInfo;
+
+public class Info
+{
+    public const string CoreVersion = "ALPHA-2023721-0001";
+    public const string LauncherVersion = "ALPHA 0.1";
+    public const string License = "MIT";
+}
