4f251482ce58955c190750c0601faecd5f12bb7f
PowerAngelXD <692732163@qq.com>
2023-07-24 14:28:21 +0800

    Update: Utils的功能函数更新

---
+++ Utils/SLOutput.cs
@@ -0,0 +1,19 @@
+﻿namespace SLCore.Utils;
+
+using System.Drawing;
+
+public class SLOutput
+{
+    public static void Print(string text, ConsoleColor color)
+    {
+        var _default = Console.ForegroundColor; 
+        Console.ForegroundColor = color;
+        Console.WriteLine(text);
+        Console.ForegroundColor = _default;
+    }
+
+    public static void Prompt()
+    {
+        Console.Out.Write("launcher >> ");
+    }
+}
