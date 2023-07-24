16fb3ba60db695bca586871097f2d898c335003e
PowerAngelXD <692732163@qq.com>
2023-07-24 14:27:28 +0800

    Update: 增加可选错误原因

--- Errors/CommandArgumentError.cs
+++ Errors/CommandArgumentError.cs
@@ -4,6 +4,7 @@ public struct CommandArgumentErrorOptions
 {
     public static string TooManyParametersError = "对于该命令输入了过多的参数，请检查帮助手册并对该命令输入正确个数的参数";
     public static string WrongParameterError = "所输入的参数与该命令要求的不符，请查阅帮助列表以获取帮助";
+    public static string MissingParameterError = "对于该命令输入了过少的参数，请检查帮助手册并对该命令输入正确个数的参数";
 }
 
 public class CommandArgumentError : Exception
