94334c376250582612d95e1f98511b17f048a4ae
PowerAngelXD <692732163@qq.com>
2023-07-24 14:27:36 +0800

    Update: 新文件

---
+++ Core.cs
@@ -0,0 +1,14 @@
+﻿using MinecraftLaunch.Modules.Toolkits;
+
+namespace SLCore;
+
+public class Core
+{
+    public static GameCoreToolkit? CoreToolKit = null;
+
+    public static void InitCore()
+    {
+        CoreToolKit = new GameCoreToolkit(".minecraft");
+        Command.CommandPool.InitPool();
+    }
+}
