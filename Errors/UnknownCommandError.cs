9a214495eb206bd4bd5da8dcae4810050cbfdbea
PowerAngelXD <692732163@qq.com>
2023-07-24 14:28:33 +0800

    Update: 新错误UnknownCommandError

---
+++ Errors/UnknownCommandError.cs
@@ -0,0 +1,11 @@
+﻿namespace SLCore.Errors;
+
+public class UnknownCommandError : Exception
+{
+    private static string content = "输入了未知的命令, 您的输入: ";
+
+    public UnknownCommandError(string cmd)
+        : base(content + cmd)
+    {
+    }
+}
