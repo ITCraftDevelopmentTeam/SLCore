8fa5b1ca1f4dfe0bbe930b49278ff9465784fcf7
FZSGBall <692732163@qq.com>
2023-07-25 16:15:41 +0800

    Update: 新的异常

---
+++ Errors/CoreNullException.cs
@@ -0,0 +1,11 @@
+﻿namespace SLCore.Errors;
+
+public class CoreNullException: Exception
+{
+    private static string content = "使用CoreToolKit的时候未初始化或者指定了一个空CoreToolKit，您可以通过提交issue的方式来向作者反馈";
+
+    public CoreNullException()
+        : base(content)
+    {
+    }
+}
