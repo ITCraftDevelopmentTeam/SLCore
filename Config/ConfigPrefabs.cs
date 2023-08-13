921f615644934b1de77138247fa8adf7a5a757a0
FZSGBall <692732163@qq.com>
2023-08-13 16:44:39 +0800

    Update: 预制更新

--- Config/ConfigPrefabs.cs
+++ Config/ConfigPrefabs.cs
@@ -10,4 +10,16 @@ public class JavaPathConfig: IConfigSection
     public string ConfigId => "simplelauncher:config:javaPath";
     public string ConfigAliase => "javaPath";
     public string javaPath => "null";
+    public void OnChecking()
+    {
+        string rawContent = File.ReadAllText(@"./.sl_settings/config.json");
+        
+        JObject obj = JObject.Parse(rawContent);
+        JObject checker = JsonConvert.DeserializeObject<JObject>(obj?[ConfigId]?.ToString());
+        
+        if (checker?[ConfigAliase]?.ToString() == "null")
+        {
+            Utils.SLOutput.Print("检测到您还未设置游戏运行环境，请您输入 'setting' 命令进行具体的设置", ConsoleColor.Yellow);
+        }
+    }
 }
